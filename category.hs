52292f715966dc478d30b2a9bccebd381197e7f7
David Draguta <daviddraguta@gmail.com>
2020-08-16 15:30:58 -0400

    Moved everything over to category.hs

--- category.hs
+++ category.hs
@@ -1,57 +1,71 @@
---1. Category: The Essence of Composition
---1.1 Arrows as Functions
---f :: A -> B -- :: means has the type of
---g :: B -> C
---g.f
+--------------------------------------------------------------------
+----------------------------Partial----------------------------
+
+import Data.Char(toUpper)
+
+type Partial a = (Bool, a) -- a Functor on Hask. 
+
+(>=>) :: (a -> Partial b) -> (b -> Partial c) -> (a -> Partial c)  --composition, as defined for Kleisli Category
+m1 >=> m2 = \x ->
+            let (b1, v1) = m1 x
+            in
+              if b1 == False
+              then (False, undefined)
+              else
+                  let (b2, v2) = m2 v1 --v2 will be undefined by assumption if b2 is false. 
+                  in
+                    (b1 && b2, v2)
+
+ret :: a -> Partial a
+ret x = (True, x)
+
+safe_root :: Double -> Partial Double
+safe_root x = if (x >= 0) then (True, sqrt(x)) else (False, undefined)
+          
+safe_reciprocal :: Double -> Partial Double
+safe_reciprocal x = if x /= 0 then (True, 1/x) else (False, undefined)
+
+safe_root_reciprocal :: Double -> Partial Double
+safe_root_reciprocal = safe_reciprocal >=> safe_root                    
 
 
---1.2 Properties of Composition
 
---1. Composition is associative:
+--------------------------------------------------------------------
+----------------------------Isomorphisms----------------------------
+--These two give that Maybe a is isomorphic to Either () a
+maybeToEither :: Maybe a -> Either () a
+eitherToMaybe :: Either () a -> Maybe a               
 
---f :: A -> B
---g :: B -> C
---h :: C -> D
+maybeToEither a  =
+    case a of
+      Nothing -> Left ()
+      Just a -> Right a
 
---the following is pseudo-haskell, as equality is not defined for functions
---h . (g . f) == (h . g) . f == h . g . f  
+eitherToMaybe a =
+    case a of
+      Left () -> Nothing
+      Right a -> Just a
 
---2. For every object A in a category C there exists a morphism i_A: A -> A:
+                
+data Shape = Circle Float | Rect Float Float
 
---id :: a -> a declaration, names of concrete type is capitalized, names of type variables are lower case, in this case we defined a polymorphic function that takes an object of any type to itself.
---id x = x definition
---f . id == id . f == f
+area :: Shape -> Float
+area (Circle r) = pi * r * r
+area (Rect d h) = d * h
 
---1.3 Composition is the Essence of Programming
+--These two give that Either a a is isomorphic to (Bool, a),
+--Or that a + a is isomorphic to 2 x a, where we see 2 as Bool.
+eitherToPair :: Either a a -> (Bool, a)
+pairToEither :: (Bool, a) -> Either a a
 
---1.4 Challenges
-
---First 3 exercises, look in identity.rkt file.
-
---4. Is the world-wide web a category in any sense? Are links morphisms?
-
---  We can consider websites as objects, morphisms as links, and see if that holds.
---  i.First, for any link, you can refresh the page, so for any object, there is a link which points to itself.
---  ii.If there is a link between a site to another, and from that other to still another, then there is a link from the first to the third...
---  From ii. we get that the world-wide web is not a category, since links are not morphisms.
-
---5. Is facebook a category, with people as objects and friendships as morphisms?
-
--- No. If I am someones friend, and that other friend is someone elses friend, then I am not necessarily that third persons friend. Also, I'm never my own friend on facebook.
-
---6. When is a directed graph a category? 
-
---Let us say that for a directed graph G = {V , E}, and for v_1, v_2 in G, that v_1 v_2 are edge related e, and let us write v_1 e v_2, if there exists (v_1, v_2) in E. It is sufficient for the edge relation to be reflexive and transitive. If it is reflexive then for every v in V vev, i.e. (v, v) isin E, but that just represents the identity morphism. If it is transitive then we have v_1ev_2, v_2ev_3 => v_1ev_3, i.e. we have that diagrams commute, as required. Associativity also follows from transitivity of edge relation, as (f_AB . f_BC) . f_CD = f_AC . f_CD = f_AD = f_AB . (f_BD) = f_AB . (f_BC . f_BD). 
-
---2. Types and Functions
-
---2.1 Who Needs Types?
---2.2 Types Are About Composability 
---2.3 What are Types?
---2.4 Why do We Need a Mathematical Model?
---2.5 Pure and Dirty Functions
---2.6 Examples of Types
---2.7 Challenges
-
---1.
-
+eitherToPair Either a = 
+    case a of
+      Left a -> (False, a)
+      Right a -> (True, a) 
+                
+pairToEither (b,a) =
+    case b of
+      False -> Left a
+      True -> Right a
+      
+--------------------------------------------------------------------
