4b7932a1ad8bd9d92f2f7ffb464a46e75b11cad6
David Draguta <daviddraguta@gmail.com>
2020-08-18 01:45:30 -0400

    7.3 Functor Composition.

---
+++ category.cpp
@@ -0,0 +1,174 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iterator>
+#include <iostream>
+#include <vector>
+using namespace std; 
+//6.5 Challenges, exercise 2.
+//Haskell implementation of shape.
+//data Shape = Circle Float | Rect Float Float
+//
+//area :: Shape -> Float
+//area (Circle r) = pi * r * r
+//area (Rect d h) = d * h
+
+
+class Shape {
+public:
+  virtual double getArea() = 0;
+  virtual double circ() = 0;
+};
+
+class Rectangle: public Shape {
+private:
+  double width;
+  double height;
+public:
+  Rectangle(double w, double h){
+    width = w;
+    height = h;
+  };
+  double getArea() {
+    return (width * height);
+  };
+  double circ() {
+    return (2 * width) + (2 * height);
+  };
+};
+class Circle: public Shape{
+private:
+  double radius;
+public:
+  Circle(double r){
+    radius = r;
+  };
+
+  double getArea() {
+    return (M_PI * radius * radius);
+  };
+  double circ() {
+    return (2 * M_PI * radius);
+  };
+};
+//Either A B
+template<typename A,typename B>
+struct Either {
+  enum { isLeft, isRight } tag;
+  union { A left; B right; };
+};
+
+// Left: A -> Either A B
+template<typename A, typename B>
+Either<A,B> Left(A a){
+  Either<A, B> e;
+  e.tag = e.isLeft;
+  e.left = a;
+  return e;
+};
+
+// Right: B -> Either A B
+template<typename A, typename B>
+Either<A,B> Right(B b){
+  Either<A, B> e;
+  e.tag = e.isRight;
+  e.right = b;
+  return e;
+};
+
+int i(int n) {
+  if (n < 0) return n;
+  return n + 2;
+};
+
+Either<int, bool> _m(int n){
+  if (n == 0)
+    return Right<int, bool>(true);
+  else if(n == 1)
+    return Right<int, bool>(false);
+  else if(n >= 2)
+    return Left<int, bool>(n-2);
+  else
+    return Left<int, bool>(n);
+};
+
+      
+
+int j(bool b) {
+  return b ? 0: 1;
+};
+
+int m(Either<int, bool> const &e){
+  if(e.tag == e.isLeft)
+    return e.left;
+  else
+    return e.right ? 0: 1;
+};
+
+//implementation of optional, which is the cpp implementation of Haskell's Maybe.
+template<class T>
+class optional {
+  bool _isValid; //the tag
+  T _v;
+public:
+  optional() : _isValid(false) {}          //Nothing
+  optional(T x) : _isValid(true), _v(x) {} //Just
+  bool isValid() const { return _isValid; }
+  T val() const { return _v; }
+};
+
+//curried
+template<class A, class B>
+function<optional<B>(optional<A>)> //lift from A -> B to optional A -> optional B
+fmap(function<B(A)> f){
+  return [f](optional<A> opt) {
+    if (!opt.isValid())
+      return optional<B>{ //Nothing
+      };
+    else
+      return optional<B>{ //Just f(opt.val())
+	f(opt.val())
+      };
+  };
+}
+
+//uncurried
+//template<class A, class B>
+//optional<B>
+//fmap(function<B(A)> f, optional<A> opt){
+//  if(!.opt.isValid()) //Nothing
+//    return optional<B>{};
+//  else                //Maybe
+//    return optional<B>{ f(opt.val()) };
+//}
+
+//cpp implementation of Haskell Lisp functor:
+template<class A, class B>
+vector<B>
+fmap(function<B(A)> f, vector<A> v) {
+  vector<B> w;
+  transform(begin(v), end(v), back_inserter(w), f); //transforms applies f from begin(v) to end(v), and uses back_inserter(w) to store the result in w.
+  return w;
+}
+
+//cpp implementation of Const c a
+template<class C, class A>
+struct Const{
+  Const(C v) : _v(v) {}
+  C _v;
+};
+
+//cpp implementation of fmap for Const c a
+template<class C, class A, class B>
+Const<C, B> fmap(function<B(A)> f, Const<C, A> c) {
+  return Const<C,B>{c._v};
+}
+int main(){
+  vector<int> v{ 1, 2, 3, 4 };
+  auto w = fmap([](int i) { //lambda function
+    return i * i;
+  }, v);
+  copy(begin(w), end(w), ostream_iterator(cout, ", ")); //print result
+    
+};
--- category.hs
+++ category.hs
@@ -58,7 +58,7 @@ area (Rect d h) = d * h
 eitherToPair :: Either a a -> (Bool, a)
 pairToEither :: (Bool, a) -> Either a a
 
-eitherToPair Either a = 
+eitherToPair a = 
     case a of
       Left a -> (False, a)
       Right a -> (True, a) 
@@ -68,4 +68,60 @@ pairToEither (b,a) =
       False -> Left a
       True -> Right a
       
---------------------------------------------------------------------
+-----------------------------------------------------------------------
+----------------------------Typeclasses--------------------------------
+--typeclass defines a family of types that support a common interface
+--class Eqq a where --a is of the class Eq if it supports (==)
+--    (=*=) :: a -> a -> Bool
+--an example, we define a 2D Point:
+--data Point = Pt Float Float
+
+--now we can define equality of points:
+--instance Eqq Point where
+--    (Pt x y) =*= (Pt x' y') = x == x' && y == y' --definition of Point equality in terms of Float equality.
+
+--typeclass also defines a family of mappings of types (type constructors).
+--class Functor f where
+--    fmap :: (a -> b) -> f a -> f b
+--Thus we have an fmap for each endofunctor. In the case of Maybe, we implement the following (weird names to avoid name conflict with Prelude defined Maybe
+--instance Functor Maybee where
+--    fmap _ Nothing = Nothing
+--    fmap f (Just x) = Just (f x)
+
+--List functor
+data List a = Nil | Cons a (List a) -- Cons :: a -> List a -> List a, so Cons is a bifunctor. Nil is a 0-ary functor.
+fmap :: (a -> b) -> (List a -> List b)
+
+--We have the following implementation of fmap:        
+--Instance Functor List where
+--    fmap _ Nil = Nil
+--    (fmap f) Cons x t = Cons (f x) ((fmap f) t)
+
+--Next we consider something of the form \hom(a,-), which we can define by giving the binary operator -> one argument,
+hom a = r -> a
+--In this case we have
+fmap :: (a -> b) -> (r -> a) -> (r -> b)
+--Which can be implemented as follows, g: r->a, f: a->b =>
+instance Functor ((->) r) where
+    fmap f g = f . g
+--We could also just write
+instance Functor ((->) r) where
+    fmap = (.)
+
+--Also, we have the functor Const c -, where Const c a = Const c. It's fmap is of type:
+fmap :: (a -> b) -> Const c a -> Const c b
+--With the following implementation:
+instance Functor (Const c) where
+    fmap _ (Const x) = Const x
+
+
+-----------------------------------------------------------------------
+----------------------------Composition--------------------------------
+
+maybeTail :: [a] -> Maybe [a] --This is a composition of functors, the built-in haskell list constructor functor, then Maybe.
+maybeTail [] = Nothing        --Note: Cons is replaced by the infix : and Nil is replaced by [], so we get Cons 5 (Cons 6 Nil) = 5 : 6 : [] = [5, 6]
+maybeTail (x:xs) = Just xs               
+
+             
+
+
